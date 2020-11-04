a4c75814a99b87ccbf2749b1e14d0bfa4cfff544
Rey Walter Magbanua <walter@Reys-MacBook-Air.local>
2020-11-04 18:07:31 -0500

    Part 2

---
+++ main.js
@@ -0,0 +1,59 @@
+let myShape;
+let shapes = ["rock", "paper", "scissors"];
+let buttons = document.querySelectorAll(".optionBtn");
+
+// For loop
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(e){
+        myShape = buttons[i].value.toLowerCase();
+    });
+}
+
+// When form is submitted, do this functiom
+document.querySelector("form").addEventListener("submit", function(e){
+    
+    // Prevents the site from refreshing
+    e.preventDefault();
+    
+    console.log("Your shape: " + myShape);
+    
+    // This is a function, not a method
+    function generateNum(){
+        // returns a generated integer number between 1 and 3
+        return Math.floor(Math.random() * 3) + 1;
+    }
+    
+    // This function generates a random shape
+    function generateShape(i){
+        switch(i){
+            case 1:
+                return "rock";
+                break;
+            case 2:
+                return "paper";
+                break;
+            default:
+                return "scissors";
+                break;
+        }
+    }
+    
+    // Decide a winner
+    function startGame(x){
+        if(x == "rock" && myShape == "paper"){
+            return "Computer decided a shape: " + x + ". You won";
+        } else if(x == "paper" && myShape == "scissors") {
+            return "Computer decided a shape: " + x + ". You won";
+        } else if(x == "scissors" && myShape == "rock"){
+            return "Computer decided a shape: " + x + ". You won";
+        } else if(x == myShape){
+            return "Computer decided a shape: " + x + ". It's draw";
+        } else if(shapes.indexOf(myShape) > -1){
+            return "Computer decided a shape: " + x + ". Computer won";
+        } else {
+            return "Please choose one of the following: Rock, Paper, or Scissors";
+        }
+    }
+    
+    console.log(startGame(generateShape(generateNum())));
+});
