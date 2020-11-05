bcdf48e16f56526ae39f6734869add3ac76cdab6
Rey Walter Magbanua <walter@Reys-MacBook-Air.local>
2020-11-05 00:58:47 -0500

    Part 3

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let myShape;
 let shapes = ["rock", "paper", "scissors"];
 let buttons = document.querySelectorAll(".optionBtn");
+let images = document.querySelectorAll("img");
 
 // For loop
 for(let i = 0; i < buttons.length; i++){
@@ -11,7 +12,7 @@ for(let i = 0; i < buttons.length; i++){
 
 // When form is submitted, do this functiom
 document.querySelector("form").addEventListener("submit", function(e){
-    
+
     // Prevents the site from refreshing
     e.preventDefault();
     
@@ -41,14 +42,34 @@ document.querySelector("form").addEventListener("submit", function(e){
     // Decide a winner
     function startGame(x){
         if(x == "rock" && myShape == "paper"){
+            images[1].src = "Images/" + myShape + "Hand.png";
+            images[0].src = "Images/" + x + "Hand.png";
+            images[1].style.display = "inline-block";
+            images[0].style.display = "inline-block";
             return "Computer decided a shape: " + x + ". You won";
         } else if(x == "paper" && myShape == "scissors") {
+            images[1].src = "Images/" + myShape + "Hand.png";
+            images[0].src = "Images/" + x + "Hand.png";
+            images[1].style.display = "inline-block";
+            images[0].style.display = "inline-block";
             return "Computer decided a shape: " + x + ". You won";
         } else if(x == "scissors" && myShape == "rock"){
+            images[1].src = "Images/" + myShape + "Hand.png";
+            images[0].src = "Images/" + x + "Hand.png";
+            images[1].style.display = "inline-block";
+            images[0].style.display = "inline-block";
             return "Computer decided a shape: " + x + ". You won";
         } else if(x == myShape){
+            images[1].src = "Images/" + myShape + "Hand.png";
+            images[0].src = "Images/" + x + "Hand.png";
+            images[1].style.display = "inline-block";
+            images[0].style.display = "inline-block";
             return "Computer decided a shape: " + x + ". It's draw";
         } else if(shapes.indexOf(myShape) > -1){
+            images[1].src = "Images/" + myShape + "Hand.png";
+            images[0].src = "Images/" + x + "Hand.png";
+            images[1].style.display = "inline-block";
+            images[0].style.display = "inline-block";
             return "Computer decided a shape: " + x + ". Computer won";
         } else {
             return "Please choose one of the following: Rock, Paper, or Scissors";
